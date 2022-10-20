38e7aeb736eb3d2c5586da61275d9722072ce9c1
unknown <rakelms90@gmail.com>
2022-10-20 07:56:20 +0200

    calendar with problem creatin event

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { calendarSlice } from './calendar/calendarSlice';
+import { uiSlice } from './ui/uiSlice';
+import { authSlice } from './auth/authSlice';
+
+export const store = configureStore({
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+    reducer: {
+        ui: uiSlice.reducer,
+        calendar: calendarSlice.reducer,
+        auth: authSlice.reducer,
+    }
+});
